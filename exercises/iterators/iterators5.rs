15faa6405481d68723df42d64254c972e92fa699
Bruno Alphonsus de Oliveira Nascimento <caputchinefrobles@yahoo.com.br>
2023-01-13 22:08:10 -0300

    threads1

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -10,7 +10,7 @@
 //
 // Make the code compile and the tests pass.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
 
@@ -34,7 +34,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.iter().filter(|m| m.1 == &value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -53,7 +53,12 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection
+        .into_iter()
+        .map(|m| m.values())
+        .flatten()
+        .filter(|v| v == &&value)
+        .count()
 }
 
 #[cfg(test)]
